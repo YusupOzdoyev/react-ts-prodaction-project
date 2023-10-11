093d7b276a633de2ee00187de228dded445ae225
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2023-10-11 22:15:25 +0300

    work in psd architecture and add absolute imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,13 @@
-import React, {useState, Suspense, useContext } from "react";
+import React, { Suspense } from "react";
 import './styles/index.scss';
 import { Route, Routes, Link } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPages/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPages";
+import { MainPage } from "pages/MainPage";
+import { classNames } from "shared/lib/classNames/classNames";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
-  const bool = true;
 
   return (
     <div className={classNames('app', {}, [theme])}>
@@ -17,8 +16,8 @@ const App = () => {
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={'/about'} element={<AboutPageAsync />} />
-          <Route path={'/'} element={<MainPageAsync />} />
+          <Route path={'/about'} element={<AboutPage />} />
+          <Route path={'/'} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
