44eb5bae240e85bf3e03a0a6b02732e1dcb8ff80
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2023-10-18 22:46:39 +0300

    add babel and babel-extract-plugin for example

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,10 +4,31 @@ import { BuildOptions } from "./types/config";
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   
-const svgLoader = {
-  test: /\.svg$/,
-  use: ['@svgr/webpack'],
-}
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  }
+
+  const babelLoader = {
+    test: /\.(js||jsx||tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        "plugins": [
+          [
+            "i18next-extract", 
+             
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValues: true
+            }
+          ]
+        ]
+      }
+    }
+  }
 
   const cssLoader =  {
     test: /\.s[ac]ss$/i,
@@ -43,12 +64,13 @@ const svgLoader = {
       },
     ],
   }
+      
     
-  
 
   return [
     fileLoader,
     svgLoader,
+    babelLoader,
     typescriptLoader,
     cssLoader,
   ]
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -11,11 +11,12 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
     }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
+      filename: 'css/[name].[contenth ash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ]
 }
