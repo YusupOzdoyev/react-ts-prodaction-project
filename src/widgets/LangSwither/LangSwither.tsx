bc646c6272a39dcb974a4ef815c5881601cd2d75
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2023-10-20 23:56:14 +0300

    add stylelint and add plugin for internalise i18n

--- src/widgets/LangSwither/LangSwither.tsx
+++ src/widgets/LangSwither/LangSwither.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import cls from './LangSwither.module.scss';
 
 interface LangSwitherProps {
   className?: string;
@@ -15,7 +14,7 @@ export const LangSwither = ({ className }: LangSwitherProps) => {
 
     return (
         <Button
-            className={classNames(cls.LangSwither, {}, [className])}
+            className={classNames('', {}, [className])}
             theme={ThemeButton.CLEAR}
             onClick={toggle}
         >
