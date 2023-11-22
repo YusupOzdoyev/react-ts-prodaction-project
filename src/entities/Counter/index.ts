009c9668bb9d2ec4db40c367af6413382cff9274
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2023-11-22 23:56:52 +0300

    addredux store, add first entity

---
+++ src/entities/Counter/index.ts
@@ -0,0 +1,9 @@
+import { counterReducer } from './ui/model/slice/CounterSlice';
+import { Counter } from './ui/Counter';
+import type { CounterSchema } from './ui/model/types/counterSchema';
+
+export {
+    counterReducer,
+    Counter,
+    CounterSchema,
+};
