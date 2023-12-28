76e602c76e13b67c7aa460465c5cd5e52e8d5778
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2023-12-28 20:56:44 +0300

    add components creator

---
+++ .vscode/cch-template/React FC/{{pascalCase}}/{{pascalCase}}.test.tsx
@@ -0,0 +1,10 @@
+// import { render, screen } from '@testing-library/react';
+// import { {{pascalCase}} } from 'shared/ui/{{pascalCase}}/{{pascalCase}}';
+
+// describe('{{pascalCase}}', () => {
+//     test('Test render', () => {
+        
+//         expect(screen.getByText('TEST')).toBeInTheDocument();
+//     });
+
+// });
